6b4d4190af25210370f8c40efae1f774790fd06c
KimHyeonSu <hirobin2001@gmail.com>
2025-10-23 13:01:54 +0900

    장바구니 상품 목록 구현

--- routes/carts.js
+++ routes/carts.js
@@ -1,25 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const dotenv = require('dotenv');
-
-dotenv.config();
+const {addToCart, getCartItems, removeCartItem} = require('../controller/CartController');
 
 router.use(express.json());
 
-router.post('/', (req, res) => {
-    res.json('장바구니 담기');
-});
+router.post('/', addToCart);
+router.get('/', getCartItems);
+router.delete('/:id', removeCartItem);
 
-router.get('/', (req, res) => {
-    res.json('장바구니 조회');
-});
-
-router.delete('/:bookId', (req, res) => {
-    res.json('장바구니 도서 삭제');
-});
-
-// router.get('/', (req, res) => {
-//     res.json('장바구니에서 선택한 주문 예상 상품 목록 조회');
-// });
 
 module.exports = router;
